3f4edce6bb050b6a138fa24b0d8c57184086e878
Patrick <patrickyong@gmail.com>
2022-05-06 17:51:44 +0800

    Added GCP Secret Manager methods

--- main.py
+++ main.py
@@ -3,6 +3,45 @@ import sys
 
 from pyjson5 import pyjson5
 
+def create_secret(project_id, secret_id):
+    from google.cloud import secretmanager
+    client = secretmanager.SecretManagerServiceClient()
+    project_detail = f"projects/{project_id}"
+    response = client.create_secret(
+        request={
+            "parent": project_detail,
+            "secret_id": secret_id,
+            "secret": {"replication": {"automatic": {}}},
+        }
+    )
+    return response
+
+def create_secret_version(project_id, secret_id, data):
+    from google.cloud import secretmanager
+    client = secretmanager.SecretManagerServiceClient()
+    parent = client.secret_path(project_id, secret_id)
+    response = client.add_secret_version(
+        request={"parent": parent, "payload": {"data": data.encode("UTF-8")}}
+    )
+    return response
+
+def get_secret_data(project_id, secret_id, version_id):
+    from google.cloud import secretmanager
+    client = secretmanager.SecretManagerServiceClient()
+    secret_detail = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
+    response = client.access_secret_version(request={"name": secret_detail})
+    data = response.payload.data.decode("UTF-8")
+    print("Data: {}".format(data))
+    return response
+
+def secret(request):
+    project_id=''
+    secret_id='secret1'
+    data='This is the data stored in this secret'
+    version_id=1
+    create_secret(project_id,secret_id)
+    create_secret_version(project_id,secret_id,data)
+    get_secret_data(project_id,secret_id,version_id)
 
 # https://stackoverflow.com/questions/18495098/python-check-if-an-object-is-a-list-of-strings
 
